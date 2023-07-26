deff0820930f8329ca8de047073505ed0ffdfd61
HYEONP--Park <hyeonpaaark@gmail.com>
2023-07-26 21:20:19 +0900

    first commit

---
+++ src/programmers_0726.java
@@ -0,0 +1,26 @@
+/**
+ * programmers_0726
+ * 배열 회전시키기
+ */
+public class programmers_0726 {
+    public static void main(String[] args) {
+        // 입출력 예
+        // int[] arr1 = { 1, 2, 3 };
+        int[] arr1 = { 4, 455, 6, 4, -1, 45, 6 };
+        int[] answer = new int[arr1.length];
+
+        if (type == "left") {
+            for (int i = 1; i < arr1.length; i++) {
+                answer[i - 1] = arr1[i];
+            }
+            answer[arr1.length - 1] = arr1[0];
+        } else {
+            for (int i = 1; i < arr1.length; i++) {
+                answer[i + 1] = arr1[i];
+            }
+            answer[0] = arr1[arr1.length - 1];
+        }
+
+    }
+
+}
