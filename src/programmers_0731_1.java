329f4f25a2b5d8f1e08922a301efbf07b1b31939
HYEONP--Park <hyeonpaaark@gmail.com>
2023-07-31 14:15:10 +0900

    programmers_0731_세균증식

---
+++ src/programmers_0731_1.java
@@ -0,0 +1,14 @@
+public class programmers_0731_1 {
+    // 코딩테스트 입문 > 세균 증식
+
+    public static void main(String[] args) {
+        int n = 7;
+        int t = 15;
+
+        int answer = n;
+
+        for (int i = 0; i < t; i++) {
+            answer *= 2;
+        }
+    }
+}
