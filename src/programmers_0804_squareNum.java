29cde95cdef49e79bed6bca516697f88d62e2808
HYEONP--Park <hyeonpaaark@gmail.com>
2023-08-04 22:58:59 +0900

    programmers_0804

---
+++ src/programmers_0804_squareNum.java
@@ -0,0 +1,11 @@
+public class programmers_0804_squareNum {
+    public static void main(String[] args) {
+        // int n = 144;
+        int n = 976;
+
+        if (Math.sqrt(n) % 1 == 0)
+            1;
+        else
+            2;
+    }
+}
