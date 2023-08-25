4fb7fc29d608e1ffc3b6c88b8df7a42f1da55895
HYEON-Park <hyeonpaaark@gmail.com>
2023-08-25 16:33:34 +0900

    프로그래머스 공던지기

--- src/programmers_공던지기.java
+++ src/programmers_공던지기.java
@@ -1,6 +1,9 @@
 public class programmers_공던지기 {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4};
-        
+        int[] numbers = { 1, 2, 3, 4, 5, 6 };
+        int k = 5;
+
+        System.out.println(numbers[numbers[2 * (k - 1) % numbers.length]]);
+
     }
 }
