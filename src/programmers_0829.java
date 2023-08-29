7a4e2baf5c755c7bc2a9fcc33c46394cc3355833
HYEON-Park <hyeonpaaark@gmail.com>
2023-08-29 11:54:27 +0900

    프로그래머스 최댓값만들기(1)

---
+++ src/programmers_0829.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * 최댓값 만들기 (1)
+ */
+public class programmers_0829 {
+    public static void main(String[] args) {
+        int[] numbers = { 0, 31, 24, 10, 1, 9 };
+
+        // Arrays.sort(numbers);
+
+        Arrays.sort(numbers);
+
+        int size = numbers.length;
+
+        // return numbers[size-1] * numbers[size-2];
+
+    }
+}
