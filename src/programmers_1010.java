81de98ec32ced5f227957220c9afc930834efef6
HYEON-Park <hyeonpaaark@gmail.com>
2023-10-10 13:56:18 +0900

    프로그래머스 종이자르기

---
+++ src/programmers_1010.java
@@ -0,0 +1,11 @@
+public class programmers_1010 {
+    /**
+     * 종이 자르기
+     */
+    public static void main(String[] args) {
+        int M = 1;
+        int N = 1;
+        int result = (M - 1) + ((N - 1) * M);
+
+    }
+}
