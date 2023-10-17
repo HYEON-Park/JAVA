7f004ffa3ab81f2ee9aa9b985800c68f8f47efdc
HYEON-Park <hyeonpaaark@gmail.com>
2023-10-17 22:22:41 +0900

    프로그래머스 치킨 쿠폰

---
+++ src/programmers_1017.java
@@ -0,0 +1,13 @@
+/**
+ * programmers_1017
+ * 
+ * 치킨 쿠폰
+ */
+public class programmers_1017 {
+
+    public static void main(String[] args) {
+        int chicken = 100;
+        int result = 0;
+
+    }
+}
