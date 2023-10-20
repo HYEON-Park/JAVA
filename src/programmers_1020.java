30a7259dee4741ef7739b87a0f975adf60ad59e7
HYEON-Park <hyeonpaaark@gmail.com>
2023-10-20 17:22:20 +0900

    프로그래머스 직사각형 넓이구하기

---
+++ src/programmers_1020.java
@@ -0,0 +1,19 @@
+public class programmers_1020 {
+    public static void main(String[] args) {
+        int[][] dots = { { 1, 1 }, { 2, 1 }, { 2, 2 }, { 1, 2 } };
+        int result = 0;
+
+        int w = 0;
+        int h = 0;
+        int x = dots[0][0];
+        int y = dots[0][1];
+        for (int i = 1; i < dots.length; i++) {
+            if (x != dots[i][0])
+                w = Math.abs(x - dots[i][0]);
+            if (y != dots[i][1])
+                h = Math.abs(y - dots[i][1]);
+        }
+
+        result = w * h;
+    }
+}
