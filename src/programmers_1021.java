e6b5c1d4650a955a0c02e8fb96ba4a895451fef3
HYEON-Park <hyeonpaaark@gmail.com>
2023-10-23 16:57:20 +0900

    프로그래머스 다항식 더하기

---
+++ src/programmers_1021.java
@@ -0,0 +1,15 @@
+public class programmers_1021 {
+    public static void main(String[] args) {
+        /**
+         * 다항식 더하기
+         */
+        String polynomial = "3x + 7 + x";
+
+        polynomial = polynomial.trim();
+        String[] arr = polynomial.split(polynomial);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println();
+        }
+    }
+}
